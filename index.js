2ec72ddb165225bd10d402b2e1a1b0dcbb94745c
Vincent Ambrosio <vince.ambrosio@gmail.com>
2020-09-22 13:45:43 -0400

    added notes in js doc

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 $(function() {
-  
+  //js-shopping-list-form //in form html element
+
+  //"#shopping-list-entry" // id of text input form
+
+  //"#shopping-item-delete" //delete button class
+
+  //"#shopping-item-toggle" // check button class //adds a strikethrough text
 });
