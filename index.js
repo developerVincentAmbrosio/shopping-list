97a9d447e4d404f72d0d2f3fa745877b284b3cb4
Vincent Ambrosio <vince.ambrosio@gmail.com>
2020-09-24 11:37:16 -0400

    wrote list item delete and checked callback functions

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 $(function() {
+  $event.stopPropagation();
+  //delete button class
+  $(".shopping-item-delete").click(function(event) {
+    this.remove("li");
+  });
+
+  // check button class //adds a strikethrough text
+  $(".shopping-item-toggle").click(function(event) {
+    this.toggleClass(".shopping-item__checked")
+  });
   //js-shopping-list-form //in form html element
+  
+  //".shopping-list-entry" // id of text input form
 
-  //"#shopping-list-entry" // id of text input form
 
-  //"#shopping-item-delete" //delete button class
-
-  //"#shopping-item-toggle" // check button class //adds a strikethrough text
 });
