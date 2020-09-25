429be4b079191b8ed29c594a6e8a49f53f883484
Vincent Ambrosio <vince.ambrosio@gmail.com>
2020-09-25 13:48:08 -0400

    worked on check button feature

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 $(function() {
-  $event.stopPropagation();
-  $event.stopDefault();
+//  $event.stopPropagation();
+
   //delete an item
   $(".shopping-item-delete").click(function(event) {
-    this.remove("li");
+    $(this).closest("li").remove();
+//    console.log($(this).closest("li"));
   });
 
   // check button 
   $(".shopping-item-toggle").click(function(event) {
-    this.toggleClass(".shopping-item__checked")
+    $(this).closest(".shopping-item").toggleClass("shopping-item__checked");
+  //  console.log($(this).closest(".shopping-item"));
   });
   
   // add an item
-  $("#shopping-list-entry").submit(function(event) {
-    const newListItem = $this; //keystrokes from submitted form
+  //event.preventDefault()
+  $("form").submit(function(event) {
+    const newListItem = $this.val(text); //keystrokes from submitted form
 
     $("ul").append("<li>" + newListItem + "<li>");
   });
