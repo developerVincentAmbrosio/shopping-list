0a85b8afa92f4f29637bbadfb8a904a98be08e5b
Vincent Ambrosio <vince.ambrosio@gmail.com>
2020-09-28 12:20:01 -0400

    made add item less redundant

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 $(function() {
+  
+  // add an item
+  $("form").submit(function(event) {
+    event.preventDefault()
+    const newListItem = $("#shopping-list-entry").val(); //keystrokes from submitted form
+    $(".shopping-list").append('<li><span class="shopping-item">' + newListItem + '</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>');
+  });
 
   //delete an item
   $(".shopping-item-delete").click(function(event) {
@@ -8,21 +15,6 @@ $(function() {
   // check button 
   $(".shopping-item-toggle").click(function(event) {
     $(this).parent().prev().toggleClass("shopping-item__checked");
-  });
-
-  // add an item
-  $("form").submit(function(event) {
-    event.preventDefault()
-    const newListItem = $("#shopping-list-entry").val(); //keystrokes from submitted form
-    $(".shopping-list").append('<li><span class="shopping-item">' + newListItem + '</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>');
-
-    $(".shopping-item-delete").click(function(event) {
-      $(this).closest("li").remove();
-    });
-  
-    $(".shopping-item-toggle").click(function(event) {
-      $(this).parent().prev().toggleClass("shopping-item__checked");
-    });
   });
 
 });
