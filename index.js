74e749781e4963fae5bdc3edc82937bec337c249
Vincent Ambrosio <vince.ambrosio@gmail.com>
2020-09-28 14:17:19 -0400

    added event delegation to add item feature

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 $(function() {
-  
+
   // add an item
   $("form").submit(function(event) {
     event.preventDefault()
     const newListItem = $("#shopping-list-entry").val(); //keystrokes from submitted form
-    $(".shopping-list").append('<li><span class="shopping-item">' + newListItem + '</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>');
+    $("ul", ".shopping-list").append('<li><span class="shopping-item">' + newListItem + '</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>');
   });
 
   //delete an item
