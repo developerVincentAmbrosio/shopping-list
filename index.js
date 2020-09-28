f98bedf746019b050c3e47641f80e80c36d9a320
Vincent Ambrosio <vince.ambrosio@gmail.com>
2020-09-28 12:07:35 -0400

    finished add item feature

--- index.js
+++ index.js
@@ -14,17 +14,15 @@ $(function() {
   $("form").submit(function(event) {
     event.preventDefault()
     const newListItem = $("#shopping-list-entry").val(); //keystrokes from submitted form
-    
     $(".shopping-list").append('<li><span class="shopping-item">' + newListItem + '</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>');
-    
-    $(".shopping-item-toggle").click(function(event) {
-      $(this).parent().prev().toggleClass("shopping-item__checked");
-    });
 
     $(".shopping-item-delete").click(function(event) {
       $(this).closest("li").remove();
     });
-    //alert(newListItem); 
+  
+    $(".shopping-item-toggle").click(function(event) {
+      $(this).parent().prev().toggleClass("shopping-item__checked");
+    });
   });
 
 });
