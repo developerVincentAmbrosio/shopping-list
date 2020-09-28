68ac9644e6170baf155bafd65045bb1b9ec14a14
Vincent Ambrosio <vince.ambrosio@gmail.com>
2020-09-28 15:05:16 -0400

    fixed event delagation on all check, delete and add features

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 $(function() {
 
   // add an item
-  $("form").on("submit", function(event) {
+  $("form").submit(function(event) {
     event.preventDefault()
     const newListItem = $("#shopping-list-entry").val(); //keystrokes from submitted form
     $("ul").append('<li><span class="shopping-item">' + newListItem + '</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>');
   });
 
   //delete an item
-  $(".shopping-item-delete").click(function(event) {
+  $("ul").on("click", ".shopping-item-delete", function(event) {
     $(this).closest("li").remove();
   });
 
   // check button 
-  $(".shopping-item-toggle").click(function(event) {
+  $("ul").on("click", ".shopping-item-toggle", function(event) {
     $(this).parent().prev().toggleClass("shopping-item__checked");
   });
 
